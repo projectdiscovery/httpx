dff46646da7ed63e801f48b104ca738149558393
Dothelm <49758889+bimkos@users.noreply.github.com>
2020-07-14 22:27:21 +0300

    Update title.go

--- common/httpx/title.go
+++ common/httpx/title.go
@@ -9,7 +9,7 @@ import (
 
 // ExtractTitle from a response
 func ExtractTitle(r *Response) string {
-	var re = regexp.MustCompile(`(?m)<\s*title*>(.*?)<\s*/\s*title>`)
+	var re = regexp.MustCompile(`(?m)<\s*title *>(.*?)<\s*/\s*title>`)
 	for _, match := range re.FindAllString(r.Raw, -1) {
 		return html.UnescapeString(trimTitleTags(match))
 	}
