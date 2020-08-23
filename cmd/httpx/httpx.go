9a3c399a306db9819af315aaa105baabbaf6ec26
bauthard <8293321+bauthard@users.noreply.github.com>
2020-08-24 02:37:55 +0530

    version update

--- cmd/httpx/httpx.go
+++ cmd/httpx/httpx.go
@@ -639,11 +639,11 @@ const banner = `
   / __ \/ __/ __/ __ \|   / 
  / / / / /_/ /_/ /_/ /   |  
 /_/ /_/\__/\__/ .___/_/|_|  
-             /_/              v1           
+             /_/              v1.0
 `
 
 // Version is the current version of httpx
-const Version = `0.0.8`
+const Version = `1.0.0`
 
 // showBanner is used to show the banner to the user
 func showBanner() {
