3b4e589683c208ece1a20fa24e9c6457b0108242
bauthard <8293321+bauthard@users.noreply.github.com>
2020-09-01 10:35:58 +0530

    Revert "Fixed method printing issue "

--- cmd/httpx/httpx.go
+++ cmd/httpx/httpx.go
@@ -118,10 +118,7 @@ func main() {
 	scanopts.HTTP2Probe = options.HTTP2Probe
 	scanopts.OutputMethod = options.OutputMethod
 	if len(scanopts.Methods) > 0 {
-		// Print methods if only more than 1 are given
-		if len(scanopts.Methods) > 1 {
-			scanopts.OutputMethod = true
-		}
+		scanopts.OutputMethod = true
 
 		// Try to create output folder if it doesnt exist
 		if options.StoreResponse && !fileutil.FolderExists(options.StoreResponseDir) {
