f38a58ba42f1f211a31df4180fbaeffcb148b186
Mzack9999 <mzack9999@protonmail.com>
2020-09-14 02:57:01 +0200

    adding output ip

--- common/cache/dialer.go
+++ common/cache/dialer.go
@@ -5,7 +5,11 @@ import (
 	"net"
 	"strings"
 	"time"
+
+	"github.com/coocood/freecache"
 )
+
+var dialerHistory *freecache.Cache
 
 // NoAddressFoundError occurs when no addresses are found for the host
 type NoAddressFoundError struct{}
@@ -23,6 +27,7 @@ func NewDialer(options Options) (DialerFunc, error) {
 	if err != nil {
 		return nil, err
 	}
+	dialerHistory = freecache.NewCache(options.CacheSize * 1024 * 1024)
 	dialer := &net.Dialer{
 		Timeout:   10 * time.Second,
 		KeepAlive: 10 * time.Second,
@@ -32,7 +37,8 @@ func NewDialer(options Options) (DialerFunc, error) {
 		separator := strings.LastIndex(address, ":")
 
 		// we need to filter out empty records
-		ips, err := cache.Lookup(address[:separator])
+		hostname := address[:separator]
+		ips, err := cache.Lookup(hostname)
 		var finalIps []string
 		for _, ip := range ips {
 			if ip != "" {
@@ -45,9 +51,19 @@ func NewDialer(options Options) (DialerFunc, error) {
 		for _, ip := range ips {
 			conn, err = dialer.DialContext(ctx, network, ip+address[separator:])
 			if err == nil {
+				dialerHistory.Set([]byte(hostname), []byte(ip), 0)
 				break
 			}
 		}
 		return
 	}, nil
+}
+
+// GetDialedIP returns the ip dialed by the HTTP client
+func GetDialedIP(hostname string) string {
+	v, err := dialerHistory.Get([]byte(hostname))
+	if err != nil {
+		return ""
+	}
+	return string(v)
 }
