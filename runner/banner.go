d258ac3a40ccfe9132878257366ac39b1e34a00b
sandeep <8293321+ehsandeep@users.noreply.github.com>
2021-05-09 16:21:31 +0530

    version update

--- runner/banner.go
+++ runner/banner.go
@@ -8,11 +8,11 @@ const banner = `
   / __ \/ __/ __/ __ \|   /
  / / / / /_/ /_/ /_/ /   |
 /_/ /_/\__/\__/ .___/_/|_|
-             /_/              v1.0.6
+             /_/              v1.0.7
 `
 
 // Version is the current version of httpx
-const Version = `v1.0.6`
+const Version = `v1.0.7`
 
 // showBanner is used to show the banner to the user
 func showBanner() {
