934e10401521272223c043b7232faa634fedc1fc
Ice3man543 <nizamulrana@gmail.com>
2021-05-23 02:02:02 +0530

    Removed some unnecessary data from json

--- common/httpx/tls.go
+++ common/httpx/tls.go
@@ -2,11 +2,7 @@ package httpx
 
 import (
 	"bytes"
-	"crypto/md5"
-	"crypto/sha1"
 	"crypto/sha256"
-	"crypto/tls"
-	"crypto/x509"
 	"encoding/hex"
 	"errors"
 	"fmt"
@@ -15,24 +11,16 @@ import (
 
 // TLSData contains the relevant Transport Layer Security information
 type TLSData struct {
-	TLSVersion               string              `json:"tls_version,omitempty"`
-	CipherSuite              string              `json:"cipher_suite,omitempty"`
-	NegotiatedProtocol       string              `json:"negotiated_protocol,omitempty"`
-	ExtensionServerName      string              `json:"extension_server_name,omitempty"`
-	OCSPResponse             []byte              `json:"ocsp_response,omitempty"`
-	DNSNames                 []string            `json:"dns_names,omitempty"`
-	Emails                   []string            `json:"emails,omitempty"`
-	CommonName               []string            `json:"common_name,omitempty"`
-	Organization             []string            `json:"organization,omitempty"`
-	IssuerCommonName         []string            `json:"issuer_common_name,omitempty"`
-	IssuerOrg                []string            `json:"issuer_organization,omitempty"`
-	FingerprintMD5           string              `json:"fingerprint_md5,omitempty"`
-	FingerprintSHA1          string              `json:"fingerprint_sha1,omitempty"`
-	FingerprintSHA256        string              `json:"fingerprint_sha256,omitempty"`
-	FingerprintMD5OpenSSL    string              `json:"fingerprint_md5_openssl,omitempty"`
-	FingerprintSHA1OpenSSL   string              `json:"fingerprint_sha1_openssl,omitempty"`
-	FingerprintSHA256OpenSSL string              `json:"fingerprint_sha256_openssl,omitempty"`
-	RawCertificateChain      []*x509.Certificate `json:"raw_certificate_chain,omitempty"`
+	TLSVersion               string   `json:"tls_version,omitempty"`
+	ExtensionServerName      string   `json:"extension_server_name,omitempty"`
+	DNSNames                 []string `json:"dns_names,omitempty"`
+	Emails                   []string `json:"emails,omitempty"`
+	CommonName               []string `json:"common_name,omitempty"`
+	Organization             []string `json:"organization,omitempty"`
+	IssuerCommonName         []string `json:"issuer_common_name,omitempty"`
+	IssuerOrg                []string `json:"issuer_organization,omitempty"`
+	FingerprintSHA256        string   `json:"fingerprint_sha256,omitempty"`
+	FingerprintSHA256OpenSSL string   `json:"fingerprint_sha256_openssl,omitempty"`
 }
 
 // TLSGrab fills the TLSData
@@ -47,25 +35,15 @@ func (h *HTTPX) TLSGrab(r *http.Response) *TLSData {
 		tlsdata.Organization = append(tlsdata.Organization, cert.Subject.Organization...)
 		tlsdata.IssuerOrg = append(tlsdata.IssuerOrg, cert.Issuer.Organization...)
 		tlsdata.IssuerCommonName = append(tlsdata.IssuerCommonName, cert.Issuer.CommonName)
-		tlsdata.CipherSuite = tls.CipherSuiteName(r.TLS.CipherSuite)
-		tlsdata.NegotiatedProtocol = r.TLS.NegotiatedProtocol
 		tlsdata.ExtensionServerName = r.TLS.ServerName
-		tlsdata.OCSPResponse = r.TLS.OCSPResponse
 		if v, ok := tlsVersionStringMap[r.TLS.Version]; ok {
 			tlsdata.TLSVersion = v
 		}
 
-		if fingerprintMD5, fingerprintSHA1, fingerprintSHA256, err := calculatFingerprints(r); err == nil {
-			tlsdata.FingerprintMD5 = asHex(fingerprintMD5)
-			tlsdata.FingerprintSHA1 = asHex(fingerprintSHA1)
+		if fingerprintSHA256, err := calculateFingerprints(r); err == nil {
 			tlsdata.FingerprintSHA256 = asHex(fingerprintSHA256)
-			tlsdata.FingerprintMD5OpenSSL = asOpenSSL(fingerprintMD5)
-			tlsdata.FingerprintSHA1OpenSSL = asOpenSSL(fingerprintSHA1)
 			tlsdata.FingerprintSHA256OpenSSL = asOpenSSL(fingerprintSHA256)
 		}
-
-		tlsdata.RawCertificateChain = r.TLS.PeerCertificates
-
 		return &tlsdata
 	}
 	return nil
@@ -79,17 +57,13 @@ var tlsVersionStringMap = map[uint16]string{
 	0x0304: "TLS13",
 }
 
-func calculatFingerprints(r *http.Response) (fingerprintMD5, fingerprintSHA1, fingerprintSHA256 []byte, err error) {
+func calculateFingerprints(r *http.Response) (fingerprintSHA256 []byte, err error) {
 	if len(r.TLS.PeerCertificates) == 0 {
 		err = errors.New("no certificates found")
 		return
 	}
 
 	cert := r.TLS.PeerCertificates[0]
-	dataMD5 := md5.Sum(cert.Raw)
-	fingerprintMD5 = dataMD5[:]
-	dataSHA1 := sha1.Sum(cert.Raw)
-	fingerprintSHA1 = dataSHA1[:]
 	dataSHA256 := sha256.Sum256(cert.Raw)
 	fingerprintSHA256 = dataSHA256[:]
 	return
