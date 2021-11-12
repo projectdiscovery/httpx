4d4db87346bb6452b1f989d6f441e3bd5b1ea065
LuitelSamikshya <luitelsamikshya1@gmail.com>
2021-11-11 20:51:31 -0600

    functional test

--- internal/testutils/integration.go
+++ internal/testutils/integration.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"fmt"
 )
 
 // RunNucleiAndGetResults returns a list of results for a template
@@ -32,6 +33,31 @@ func RunHttpxAndGetResults(url string, debug bool, extra ...string) ([]string, e
 		}
 	}
 	return parts, nil
+}
+func RunHttpxBinaryAndGetResults(target string, httpxBinary string, debug bool, args []string) ([]string, error) {
+	cmd := exec.Command("bash", "-c")
+	cmdLine := fmt.Sprintf(`echo %s | %s `, target, httpxBinary)
+	cmdLine += strings.Join(args, " ")
+	if debug {
+		cmdLine += " -debug"
+		cmd.Stderr = os.Stderr
+	} else {
+		cmdLine += " -silent"
+	}
+
+	cmd.Args = append(cmd.Args, cmdLine)
+	data, err := cmd.Output()
+	if err != nil {
+		return nil, err
+	}
+	parts := []string{}
+	items := strings.Split(string(data), "\n")
+	for _, i := range items {
+		if i != "" {
+			parts = append(parts, i)
+		}
+	}
+	return parts,nil
 }
 
 // TestCase is a single integration test case
