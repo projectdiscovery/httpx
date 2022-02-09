f81b28613a0073e27bb4c2e2eb67cd9087f42fe0
mzack <marco.rivoli.nvh@gmail.com>
2022-02-09 07:54:44 +0100

    Updating GH workflows + Sonar

--- cmd/functional-test/run.sh
+++ cmd/functional-test/run.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-echo 'Building functional-test binary'
-go build
+# reading os type from arguments
+CURRENT_OS=$1
 
-echo 'Building HTTPX binary from current branch'
-go build -o httpx_dev ../httpx
+if [ "${CURRENT_OS}" == "windows-latest" ];then
+    extension=.exe
+fi
 
-echo 'Installing latest release of HTTPX'
-GO111MODULE=on go build -v github.com/projectdiscovery/httpx/cmd/httpx
+echo "::group::Building functional-test binary"
+go build -o functional-test$extension
+echo "::endgroup::"
 
-echo 'Starting HTTPX functional test'
-./functional-test -main ./httpx -dev ./httpx_dev -testcases testcases.txt
+echo "::group::Building dnsx binary from current branch"
+go build -o httpx_dev$extension ../httpx
+echo "::endgroup::"
+
+echo "::group::Building latest release of dnsx"
+go build -o httpx$extension -v github.com/projectdiscovery/httpx/cmd/httpx
+echo "::endgroup::"
+
+echo 'Starting dnsx functional test'
+./functional-test$extension -main ./httpx$extension -dev ./httpx_dev$extension -testcases testcases.txt
