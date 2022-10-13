f87c89e7339690e5e64b006e9b74d8fcbb36e96e
LuitelSamikshya <luitelsamikshya1@gmail.com>
2022-10-13 10:15:47 -0500

    include dsl tag for all json keys

--- runner/types.go
+++ runner/types.go
@@ -25,51 +25,51 @@ func (o AsnResponse) String() string {
 // Result of a scan
 type Result struct {
 	Timestamp        time.Time   `json:"timestamp,omitempty" csv:"timestamp" dsl:"timestamp"`
-	ASN              interface{} `json:"asn,omitempty" csv:"asn"`
+	ASN              interface{} `json:"asn,omitempty" csv:"asn" dsl:"asn"`
 	err              error
 	CSPData          *httpx.CSPData    `json:"csp,omitempty" csv:"csp" dsl:"csp"`
-	TLSData          *clients.Response `json:"tls,omitempty" csv:"tls"`
+	TLSData          *clients.Response `json:"tls,omitempty" csv:"tls" dsl:"tls"`
 	Hashes           map[string]string `json:"hash,omitempty" csv:"hash" dsl:"hash,header_md5,header_mmh3,header_sha256,header_simhash,body_md5,body_mmh3,body_sha256,body_simhash"`
-	ExtractRegex     []string          `json:"extract_regex,omitempty" csv:"extract_regex"`
-	CDNName          string            `json:"cdn_name,omitempty" csv:"cdn_name"`
+	ExtractRegex     []string          `json:"extract_regex,omitempty" csv:"extract_regex" dsl:"extract_regex"`
+	CDNName          string            `json:"cdn_name,omitempty" csv:"cdn_name" dsl:"cdn_name"`
 	Port             string            `json:"port,omitempty" csv:"port" dsl:"port"`
 	raw              string
 	URL              string `json:"url,omitempty" csv:"url" dsl:"url"`
 	Input            string `json:"input,omitempty" csv:"input" dsl:"input"`
 	Location         string `json:"location,omitempty" csv:"location" dsl:"location"`
-	Title            string `json:"title,omitempty" csv:"title"`
+	Title            string `json:"title,omitempty" csv:"title" dsl:"title"`
 	str              string
 	Scheme           string              `json:"scheme,omitempty" csv:"scheme" dsl:"scheme"`
-	Error            string              `json:"error,omitempty" csv:"error"`
+	Error            string              `json:"error,omitempty" csv:"error" dsl:"error"`
 	WebServer        string              `json:"webserver,omitempty" csv:"webserver" dsl:"webserver"`
 	ResponseBody     string              `json:"body,omitempty" csv:"body" dsl:"body"`
 	ContentType      string              `json:"content_type,omitempty" csv:"content_type" dsl:"content_type"`
 	Method           string              `json:"method,omitempty" csv:"method" dsl:"method"`
 	Host             string              `json:"host,omitempty" csv:"host" dsl:"host"`
 	Path             string              `json:"path,omitempty" csv:"path" dsl:"path"`
-	FavIconMMH3      string              `json:"favicon,omitempty" csv:"favicon"`
-	FinalURL         string              `json:"final_url,omitempty" csv:"final_url"`
+	FavIconMMH3      string              `json:"favicon,omitempty" csv:"favicon" dsl:"favicon"`
+	FinalURL         string              `json:"final_url,omitempty" csv:"final_url" dsl:"final_url"`
 	ResponseHeader   map[string]string   `json:"header,omitempty" csv:"header" dsl:"header"`
-	RawHeader        string              `json:"raw_header,omitempty" csv:"raw_header"`
+	RawHeader        string              `json:"raw_header,omitempty" csv:"raw_header" dsl:"raw_header"`
 	Request          string              `json:"request,omitempty" csv:"request" dsl:"request"`
 	ResponseTime     string              `json:"time,omitempty" csv:"time" dsl:"time"`
-	Jarm             string              `json:"jarm,omitempty" csv:"jarm"`
-	ChainStatusCodes []int               `json:"chain_status_codes,omitempty" csv:"chain_status_codes"`
+	Jarm             string              `json:"jarm,omitempty" csv:"jarm" dsl:"jarm"`
+	ChainStatusCodes []int               `json:"chain_status_codes,omitempty" csv:"chain_status_codes" dsl:"chain_status_codes"`
 	A                []string            `json:"a,omitempty" csv:"a" dsl:"a"`
-	CNAMEs           []string            `json:"cname,omitempty" csv:"cname"`
-	Technologies     []string            `json:"tech,omitempty" csv:"tech"`
-	Extracts         map[string][]string `json:"extracts,omitempty" csv:"extracts"`
-	Chain            []httpx.ChainItem   `json:"chain,omitempty" csv:"chain"`
+	CNAMEs           []string            `json:"cname,omitempty" csv:"cname" dsl:"cname"`
+	Technologies     []string            `json:"tech,omitempty" csv:"tech" dsl:"tech"`
+	Extracts         map[string][]string `json:"extracts,omitempty" csv:"extracts" dsl:"extracts"`
+	Chain            []httpx.ChainItem   `json:"chain,omitempty" csv:"chain" dsl:"chain"`
 	Words            int                 `json:"words" csv:"words" dsl:"words"`
 	Lines            int                 `json:"lines" csv:"lines" dsl:"lines"`
 	StatusCode       int                 `json:"status_code,omitempty" csv:"status_code" dsl:"status_code"`
 	ContentLength    int                 `json:"content_length,omitempty" csv:"content_length" dsl:"content_length"`
 	Failed           bool                `json:"failed" csv:"failed" dsl:"failed"`
-	VHost            bool                `json:"vhost,omitempty" csv:"vhost"`
-	WebSocket        bool                `json:"websocket,omitempty" csv:"websocket"`
-	CDN              bool                `json:"cdn,omitempty" csv:"cdn"`
-	HTTP2            bool                `json:"http2,omitempty" csv:"http2"`
-	Pipeline         bool                `json:"pipeline,omitempty" csv:"pipeline"`
+	VHost            bool                `json:"vhost,omitempty" csv:"vhost" dsl:"vhost"`
+	WebSocket        bool                `json:"websocket,omitempty" csv:"websocket" dsl:"websocket"`
+	CDN              bool                `json:"cdn,omitempty" csv:"cdn" dsl:"cdn"`
+	HTTP2            bool                `json:"http2,omitempty" csv:"http2" dsl:"http2"`
+	Pipeline         bool                `json:"pipeline,omitempty" csv:"pipeline" dsl:"pipeline"`
 }
 
 // function to get dsl variables from result struct
