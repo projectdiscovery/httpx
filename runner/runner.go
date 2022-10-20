857ab378cc414d971aa405b542ceb243b993e9d4
Mzack9999 <mzack9999@protonmail.com>
2022-10-20 08:33:20 +0200

    refactoring expression engine
    - Using mapstructure for uniform mapping
    - Flat key:value in expressions/variables list

--- runner/runner.go
+++ runner/runner.go
@@ -28,6 +28,7 @@ import (
 	"github.com/projectdiscovery/fastdialer/fastdialer"
 	"github.com/projectdiscovery/httpx/common/customextract"
 	"github.com/projectdiscovery/httpx/common/hashes/jarm"
+	"github.com/projectdiscovery/mapsutil"
 
 	"github.com/ammario/ipisp/v2"
 	"github.com/bluele/gcache"
@@ -608,29 +609,36 @@ func (r *Runner) RunEnumeration() {
 
 			// apply matchers and filters
 			if r.options.OutputFilterCondition != "" || r.options.OutputMatchCondition != "" {
-				respObj := make(map[string]interface{})
-				if err := decodeResponse(resp, respObj); err != nil {
+				rawMap, err := ResultToMap(resp)
+				if err != nil {
 					gologger.Warning().Msgf("Could not decode response: %s\n", err)
-				} else {
-					if r.options.OutputMatchCondition != "" {
-						res, err := dsl.EvalExpr(r.options.OutputMatchCondition, respObj)
-						if err != nil {
-							gologger.Error().Msgf("Could not evaluate match condition: %s\n", err)
+					continue
+				}
 
-						} else {
-							if res == false {
-								continue
-							}
+				flatMap := make(map[string]any)
+				mapsutil.Walk(rawMap, func(k string, v any) {
+					flatMap[k] = v
+				})
+
+				if r.options.OutputMatchCondition != "" {
+					res, err := dsl.EvalExpr(r.options.OutputMatchCondition, flatMap)
+					if err != nil {
+						gologger.Error().Msgf("Could not evaluate match condition: %s\n", err)
+						continue
+					} else {
+						if res == false {
+							continue
 						}
 					}
-					if r.options.OutputFilterCondition != "" {
-						res, err := dsl.EvalExpr(r.options.OutputFilterCondition, respObj)
-						if err != nil {
-							gologger.Error().Msgf("Could not evaluate filter condition: %s\n", err)
-						} else {
-							if res == true {
-								continue
-							}
+				}
+				if r.options.OutputFilterCondition != "" {
+					res, err := dsl.EvalExpr(r.options.OutputFilterCondition, flatMap)
+					if err != nil {
+						gologger.Error().Msgf("Could not evaluate filter condition: %s\n", err)
+						continue
+					} else {
+						if res == true {
+							continue
 						}
 					}
 				}
@@ -1232,7 +1240,7 @@ retry:
 	var serverResponseRaw string
 	var request string
 	var rawResponseHeader string
-	var responseHeader map[string]string
+	var responseHeader map[string]interface{}
 	if scanopts.ResponseInStdout {
 		serverResponseRaw = string(resp.Data)
 		request = string(requestDump)
@@ -1406,7 +1414,7 @@ retry:
 	if r.options.JSONOutput && len(scanopts.Hashes) == 0 {
 		scanopts.Hashes = "md5,mmh3,sha256,simhash"
 	}
-	var hashesMap = map[string]string{}
+	hashesMap := make(map[string]interface{})
 	if scanopts.Hashes != "" {
 		hs := strings.Split(scanopts.Hashes, ",")
 		builder.WriteString(" [")
@@ -1706,8 +1714,8 @@ func getDNSData(hp *httpx.HTTPX, hostname string) (ips, cnames []string, err err
 	return
 }
 
-func normalizeHeaders(headers map[string][]string) map[string]string {
-	normalized := make(map[string]string, len(headers))
+func normalizeHeaders(headers map[string][]string) map[string]interface{} {
+	normalized := make(map[string]interface{}, len(headers))
 	for k, v := range headers {
 		normalized[strings.ReplaceAll(strings.ToLower(k), "-", "_")] = strings.Join(v, ", ")
 	}
--- runner/types.go
+++ runner/types.go
@@ -2,20 +2,20 @@ package runner
 
 import (
 	"fmt"
-	"reflect"
-	"strings"
 	"time"
 
+	"github.com/bxcodec/faker/v4"
 	"github.com/mitchellh/mapstructure"
 	"github.com/projectdiscovery/httpx/common/httpx"
+	"github.com/projectdiscovery/mapsutil"
 	"github.com/projectdiscovery/tlsx/pkg/tlsx/clients"
 )
 
 type AsnResponse struct {
-	AsNumber  string `json:"as-number" csv:"as-number"`
-	AsName    string `json:"as-name" csv:"as-name"`
-	AsCountry string `json:"as-country" csv:"as-country"`
-	AsRange   string `json:"as-range" csv:"as-range"`
+	AsNumber  string `json:"as_number" csv:"as_number"`
+	AsName    string `json:"as_name" csv:"as_name"`
+	AsCountry string `json:"as_country" csv:"as_country"`
+	AsRange   string `json:"as_range" csv:"as_range"`
 }
 
 func (o AsnResponse) String() string {
@@ -27,97 +27,82 @@ type Result struct {
 	Timestamp        time.Time    `json:"timestamp,omitempty" csv:"timestamp"`
 	ASN              *AsnResponse `json:"asn,omitempty" csv:"asn"`
 	err              error
-	CSPData          *httpx.CSPData    `json:"csp,omitempty" csv:"csp"`
-	TLSData          *clients.Response `json:"tls,omitempty" csv:"tls"`
-	Hashes           map[string]string `json:"hash,omitempty" csv:"hash"`
-	ExtractRegex     []string          `json:"extract_regex,omitempty" csv:"extract_regex"`
-	CDNName          string            `json:"cdn_name,omitempty" csv:"cdn_name"`
-	Port             string            `json:"port,omitempty" csv:"port"`
+	CSPData          *httpx.CSPData         `json:"csp,omitempty" csv:"csp"`
+	TLSData          *clients.Response      `json:"tls,omitempty" csv:"tls"`
+	Hashes           map[string]interface{} `json:"hash,omitempty" csv:"hash"`
+	ExtractRegex     []string               `json:"extract_regex,omitempty" csv:"extract_regex"`
+	CDNName          string                 `json:"cdn_name,omitempty" csv:"cdn_name"`
+	Port             string                 `json:"port,omitempty" csv:"port"`
 	raw              string
 	URL              string `json:"url,omitempty" csv:"url"`
 	Input            string `json:"input,omitempty" csv:"input"`
 	Location         string `json:"location,omitempty" csv:"location"`
 	Title            string `json:"title,omitempty" csv:"title"`
 	str              string
-	Scheme           string              `json:"scheme,omitempty" csv:"scheme"`
-	Error            string              `json:"error,omitempty" csv:"error"`
-	WebServer        string              `json:"webserver,omitempty" csv:"webserver"`
-	ResponseBody     string              `json:"body,omitempty" csv:"body"`
-	ContentType      string              `json:"content_type,omitempty" csv:"content_type"`
-	Method           string              `json:"method,omitempty" csv:"method"`
-	Host             string              `json:"host,omitempty" csv:"host"`
-	Path             string              `json:"path,omitempty" csv:"path"`
-	FavIconMMH3      string              `json:"favicon,omitempty" csv:"favicon"`
-	FinalURL         string              `json:"final_url,omitempty" csv:"final_url"`
-	ResponseHeader   map[string]string   `json:"header,omitempty" csv:"header"`
-	RawHeader        string              `json:"raw_header,omitempty" csv:"raw_header"`
-	Request          string              `json:"request,omitempty" csv:"request"`
-	ResponseTime     string              `json:"time,omitempty" csv:"time"`
-	Jarm             string              `json:"jarm,omitempty" csv:"jarm"`
-	ChainStatusCodes []int               `json:"chain_status_codes,omitempty" csv:"chain_status_codes"`
-	A                []string            `json:"a,omitempty" csv:"a"`
-	CNAMEs           []string            `json:"cname,omitempty" csv:"cname"`
-	Technologies     []string            `json:"tech,omitempty" csv:"tech"`
-	Extracts         map[string][]string `json:"extracts,omitempty" csv:"extracts"`
-	Chain            []httpx.ChainItem   `json:"chain,omitempty" csv:"chain"`
-	Words            int                 `json:"words" csv:"words"`
-	Lines            int                 `json:"lines" csv:"lines"`
-	StatusCode       int                 `json:"status_code,omitempty" csv:"status_code"`
-	ContentLength    int                 `json:"content_length,omitempty" csv:"content_length"`
-	Failed           bool                `json:"failed" csv:"failed"`
-	VHost            bool                `json:"vhost,omitempty" csv:"vhost"`
-	WebSocket        bool                `json:"websocket,omitempty" csv:"websocket"`
-	CDN              bool                `json:"cdn,omitempty" csv:"cdn"`
-	HTTP2            bool                `json:"http2,omitempty" csv:"http2"`
-	Pipeline         bool                `json:"pipeline,omitempty" csv:"pipeline"`
+	Scheme           string                 `json:"scheme,omitempty" csv:"scheme"`
+	Error            string                 `json:"error,omitempty" csv:"error"`
+	WebServer        string                 `json:"webserver,omitempty" csv:"webserver"`
+	ResponseBody     string                 `json:"body,omitempty" csv:"body"`
+	ContentType      string                 `json:"content_type,omitempty" csv:"content_type"`
+	Method           string                 `json:"method,omitempty" csv:"method"`
+	Host             string                 `json:"host,omitempty" csv:"host"`
+	Path             string                 `json:"path,omitempty" csv:"path"`
+	FavIconMMH3      string                 `json:"favicon,omitempty" csv:"favicon"`
+	FinalURL         string                 `json:"final_url,omitempty" csv:"final_url"`
+	ResponseHeader   map[string]interface{} `json:"header,omitempty" csv:"header"`
+	RawHeader        string                 `json:"raw_header,omitempty" csv:"raw_header"`
+	Request          string                 `json:"request,omitempty" csv:"request"`
+	ResponseTime     string                 `json:"time,omitempty" csv:"time"`
+	Jarm             string                 `json:"jarm,omitempty" csv:"jarm"`
+	ChainStatusCodes []int                  `json:"chain_status_codes,omitempty" csv:"chain_status_codes"`
+	A                []string               `json:"a,omitempty" csv:"a"`
+	CNAMEs           []string               `json:"cname,omitempty" csv:"cname"`
+	Technologies     []string               `json:"tech,omitempty" csv:"tech"`
+	Extracts         map[string][]string    `json:"extracts,omitempty" csv:"extracts"`
+	Chain            []httpx.ChainItem      `json:"chain,omitempty" csv:"chain"`
+	Words            int                    `json:"words" csv:"words"`
+	Lines            int                    `json:"lines" csv:"lines"`
+	StatusCode       int                    `json:"status_code,omitempty" csv:"status_code"`
+	ContentLength    int                    `json:"content_length,omitempty" csv:"content_length"`
+	Failed           bool                   `json:"failed" csv:"failed"`
+	VHost            bool                   `json:"vhost,omitempty" csv:"vhost"`
+	WebSocket        bool                   `json:"websocket,omitempty" csv:"websocket"`
+	CDN              bool                   `json:"cdn,omitempty" csv:"cdn"`
+	HTTP2            bool                   `json:"http2,omitempty" csv:"http2"`
+	Pipeline         bool                   `json:"pipeline,omitempty" csv:"pipeline"`
 }
 
 // function to get dsl variables from result struct
-func dslVariables() []string {
-	dslVarList := []string{}
-	sanitizeTag := func(tag string) {
-		if str := strings.Split(tag, ",")[0]; str != "" {
-			dslVarList = append(dslVarList, str)
-		}
+func dslVariables() ([]string, error) {
+	fakeResult := Result{}
+	if err := faker.FakeData(&fakeResult); err != nil {
+		return nil, err
 	}
-	t := reflect.TypeOf(Result{})
-	for i := 0; i < t.NumField(); i++ {
-		field := t.Field(i)
-		if field.Tag.Get("json") != "" {
-			sanitizeTag(field.Tag.Get("json"))
-		}
-		if field.Type.Kind() == reflect.Ptr || field.Type.Kind() == reflect.Struct {
-			var types reflect.Type = field.Type
-			if field.Type.Kind() == reflect.Ptr {
-				types = field.Type.Elem()
-			}
-			if types.Kind() == reflect.Struct {
-				for j := 0; j < types.NumField(); j++ {
-					subField := types.Field(j)
-					if subField.Tag.Get("json") != "" {
-						sanitizeTag(subField.Tag.Get("json"))
-					}
-				}
-			}
-		}
+	m, err := ResultToMap(fakeResult)
+	if err != nil {
+		return nil, err
 	}
-	dslVarList = append(dslVarList, "header_md5", "header_mmh3", "header_sha256", "header_simhash", "body_md5", "body_mmh3", "body_sha256", "body_simhash")
+	vars := []string{"header_md5", "header_mmh3", "header_sha256", "header_simhash", "body_md5", "body_mmh3", "body_sha256", "body_simhash"}
+	mapsutil.Walk(m, func(k string, v any) {
+		vars = append(vars, k)
+	})
 
-	return dslVarList
+	return vars, nil
 }
 
-func decodeResponse(resp Result, respObj interface{}) error {
+func ResultToMap(resp Result) (map[string]any, error) {
+	m := make(map[string]any)
 	config := &mapstructure.DecoderConfig{
 		TagName: "json",
-		Result:  &respObj,
+		Result:  &m,
 	}
 	decoder, err := mapstructure.NewDecoder(config)
 	if err != nil {
-		return fmt.Errorf("error creating decoder: %v", err)
+		return nil, fmt.Errorf("error creating decoder: %v", err)
 	}
 	err = decoder.Decode(resp)
 	if err != nil {
-		return fmt.Errorf("error decoding: %v", err)
+		return nil, fmt.Errorf("error decoding: %v", err)
 	}
-	return nil
+	return m, nil
 }
