33691677b9c62661b791384171e9b528c50be057
Nakul Bharti <knakul853@gmail.com>
2026-09-03 07:21:19 +0700

    fix: address PR review comments

--- runner/runner_test.go
+++ runner/runner_test.go
@@ -1002,7 +1002,7 @@ func TestPlainHTTPPortStaysHTTP(t *testing.T) {
 func TestOneShotPlainHTTPKeepsItsResult(t *testing.T) {
 	var served int64
 
-	listener, err := net.Listen("tcp", "127.0.0.1:0")
+	listener, err := (&net.ListenConfig{}).Listen(context.Background(), "tcp", "127.0.0.1:0")
 	require.NoError(t, err)
 	t.Cleanup(func() { _ = listener.Close() })
 
